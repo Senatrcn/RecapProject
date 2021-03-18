1fdac7c2735b0118fbe472ffc9f19c8012f099b1
sena tercan <sena_tercan@hotmail.com>
2021-03-18 19:53:10 +0300

    10. Gün Ödev 4

--- RecapProject/Business/Abstract/ICarService.cs
+++ RecapProject/Business/Abstract/ICarService.cs
@@ -12,9 +12,10 @@ namespace RecapProject.Business.Abstract
         IResult Add(Car car);
         IResult Delete(Car car);
         IResult Update(Car car);
+        IResult GetById(int id);
         IDataResult<List<Car>> GetAll();
-        IDataResult<List<Car>> GetCarsByBrandId(int brandId);
-        IDataResult<List<Car>> GetCarsByColorId(int colorId);
+        IDataResult<List<Car>> GetByBrandId(int brandId);
+        IDataResult<List<Car>> GetByColorId(int colorId);
         IDataResult<List<CarDetailDto>> GetCarDetails();
 
     }
